2adddb4dc6fe26ed19a5a4b6b82e08b25d8de650
Ave Jonathan Cahyadi <ave.cahyadi@gmail.com>
2019-03-21 10:38:10 +0000

    bquery

--- edu/iiot-on-gcp-coursera/lab-exercises/7-big-query/1-setup.sh
+++ edu/iiot-on-gcp-coursera/lab-exercises/7-big-query/1-setup.sh
@@ -1,13 +1,13 @@
 #! /bin/bash 
 
 export CLOUDSDK_PYTHON=/usr/bin/python2.7
-# gcloud init
+gcloud init
 
 export PROJECT_ID=$(gcloud config get-value project)
 
-# gcloud pubsub topics create device-events 
+gcloud pubsub topics create device-events 
 
-# gsutil mb -l us-central1 gs://$PROJECT_ID/
+gsutil mb -l us-central1 gs://$PROJECT_ID/
 
 # simulate events 
 # re-install google cloud pubsub library used by the simulator 
@@ -17,7 +17,12 @@ sudo pip uninstall google-cloud
 sudo pip install google-cloud
 sudo pip install google-cloud-pubsub
 
-# git clone https://github.com/cagamboa123/sensor-sim.git
-# mv sensor-sim /home/ec2-user
+# generate the sample credential can be generated using 
+# "Create Service Account Key" page from here 
+# https://cloud.google.com/docs/authentication/getting-started
+export GOOGLE_APPLICATION_CREDENTIALS='sample-credentials.json'
+
+git clone https://github.com/cagamboa123/sensor-sim.git
+mv sensor-sim /home/ec2-user
 cd /home/ec2-user/sensor-sim
 python sendData.py -p $PROJECT_ID -t device-events
