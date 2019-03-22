b1bf6b3c26f79e749e39623c54cd55156e15b544
Ave Jonathan Cahyadi <ave.cahyadi@gmail.com>
2019-03-22 07:57:20 +0000

    backup repos

--- iiot-on-gcp-coursera/lab-exercises/google-repo/backup-relevant-repo.sh
+++ iiot-on-gcp-coursera/lab-exercises/google-repo/backup-relevant-repo.sh
@@ -1,6 +1,14 @@
+# clean up git folder as you can't have git subfolders in one repo
+rm -rf training-data-analyst
+rm -rf sensor-sim
+rm -rf deploymentmanager-samples
+
+#
 git clone http://github.com/GoogleCloudPlatform/training-data-analyst
 git clone https://github.com/cagamboa123/sensor-sim.git
+git clone http://github.com/GoogleCloudPlatform/deploymentmanager-samples
 
 # clean up git folder as you can't have git subfolders in one repo
 rm -rf training-data-analyst/.git
 rm -rf sensor-sim/.git
+rm -rf deploymentmanager-samples/.git
